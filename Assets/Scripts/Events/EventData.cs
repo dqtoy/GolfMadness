3411783fef580f27c2c0685795d25a9a88a03503
Alfredo <alfredo.jimenez@socialpoint.es>
2018-11-11 18:04:57 +0100

    Added event manager

---
+++ Assets/Scripts/Events/EventData.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine.Events;
+using System;
+
+public interface IEventData { }
+
+[Serializable]
+public abstract class GameEvent : UnityEvent<GameEventData>
+{
+    public void Initialize()
+    {
+        EventManager.RegisterEvent(GetEventName(), this);
+    }
+
+    public abstract string GetEventName();
+}
+
+[Serializable]
+public class ExampleEvent : GameEvent
+{
+    public static string EventName = "exampleevent";
+    public override string GetEventName() { return EventName; }
+}
+
+public interface GameEventData { }
+
+public class ExampleEventData : GameEventData
+{
+    public int x;
+    public int y;
+}
---
+++ Assets/Scripts/Events/EventManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class EventManager : MonoBehaviour
+{
+    public static EventManager Instance;
+
+    private Dictionary<string, GameEvent> _eventDictionary;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            _eventDictionary = new Dictionary<string, GameEvent>();
+        }
+        else if (Instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public static void RegisterEvent(string eventName, GameEvent gameEvent)
+    {
+        Instance._eventDictionary.Add(eventName, gameEvent);
+    }
+
+    public static void StartListening(string eventName, UnityAction<GameEventData> listener)
+    {
+        GameEvent thisEvent = null;
+        if (Instance._eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.AddListener(listener);
+        }
+
+    }
+
+    public static void StopListening(string eventName, UnityAction<GameEventData> listener)
+    {
+        if (Instance == null) return;
+        GameEvent thisEvent = null;
+        if (Instance._eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.RemoveListener(listener);
+        }
+    }
+
+    public static void TriggerEvent(string eventName, GameEventData arg = null)
+    {
+        GameEvent thisEvent = null;
+        if (Instance._eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.Invoke(arg);
+        }
+    }
+}
